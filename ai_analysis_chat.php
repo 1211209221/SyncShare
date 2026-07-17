c7c99f30058319ede8e149c12fddc725e3702372
Benjaminlim242UT244B4 <benziety.png@gmail.com>
2026-07-17 14:12:01 +0800

    final fix

--- ai_analysis_chat.php
+++ ai_analysis_chat.php
@@ -4,7 +4,7 @@ header("Content-Type: application/json");
 // ==========================
 // API KEY
 // ==========================
-$API_KEY = "AQ.Ab8RN6JRz_ovOyhJ2mGRrXq0x7StTyDXAmjpDxycpzH4HYlk7w";
+$API_KEY = "YOUR_GEMINI_API_KEY";
 //AIzaSyDLq9Ig9JU3l2CRUFv21AGl0F1Gi3FOdEM
 // ==========================
 // 1. READ INPUT
